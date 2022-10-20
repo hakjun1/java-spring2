f6deca103774fe10e1363faa2bf2cb575e008c0f
Hakjun <hiatus9014@gmail.com>
2022-10-20 13:35:17 +0900

    addAndGet()테스트코드 리펙토링

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -20,23 +20,26 @@ class UserDaoTest {
 
 
     @Test
-    void addselect() throws SQLException {
+    void addAndget() throws SQLException {
+        User user1 = new User("1","hakjun1","123");
+        User user2 = new User("2","hakjun2","456");
+        User user3 = new User("3","hakjun3","789");
+
         UserDao userDao = context.getBean("awsUserDao",UserDao.class);
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
-        String id = "7";
-        userDao.add(new User(id,"hakjun","1234"));
+        userDao.add(user1);
         assertEquals(1, userDao.getCount());
         System.out.println(userDao.getCount());
 
 
-        User user = userDao.findById("7");
-        System.out.println(user.getId());
-        System.out.println(user.getName());
-        System.out.println(user.getPassword());
-        assertEquals("hakjun",user.getName());
-        assertEquals("1234",user.getPassword());
+        User user = userDao.findById(user1.getId());
+//        System.out.println(user.getId());
+//        System.out.println(user.getName());
+//        System.out.println(user.getPassword());
+        assertEquals(user1.getName(),user.getName());
+        assertEquals(user1.getPassword(),user.getPassword());
     }
 
     @Test
