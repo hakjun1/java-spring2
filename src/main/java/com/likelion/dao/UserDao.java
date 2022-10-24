f805a30a8f8bb31fc2d107b1aea3be0625f7e1b3
Hakjun <hiatus9014@gmail.com>
2022-10-24 15:37:52 +0900

    RowMapper분리

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -1,16 +1,25 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import com.mysql.cj.result.Row;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
+    private RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            return user;
+        }
+    };
 
 
     public UserDao(DataSource dataSource) {
@@ -18,32 +27,29 @@ public class UserDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) throws SQLException {
-        this.jdbcTemplate.update("INSERT INTO users(id,name,password) values(?,?,?);",user.getId(),user.getName(),user.getPassword());
+    public void add(final User user) {
+        this.jdbcTemplate.update("INSERT INTO users(id,name,password) values(?,?,?);", user.getId(), user.getName(), user.getPassword());
     }
 
 
-    public User get(String id) throws SQLException {
+    public User get(String id)  {
         String sql = "SELECT * FROM users WHERE id =?";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-                return user;
-            }
-        };
         return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
 
     }
 
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll()  {
         this.jdbcTemplate.update("delete from users");//쿼리만 넘기는 형태
 
     }
 
-    public int getCount() throws SQLException {
+    public int getCount()  {
         return this.jdbcTemplate.queryForObject("SELECT COUNT(*) from users;", Integer.class);
+    }
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users order by id";
+        return this.jdbcTemplate.query(sql, rowMapper);
     }
 
 }
--- src/test/java/com/likelion/domain/UserDaoTest.java
+++ src/test/java/com/likelion/domain/UserDaoTest.java
@@ -4,6 +4,7 @@ import com.likelion.dao.LocalConnectionMaker;
 import com.likelion.dao.UserDao;
 import com.likelion.dao.UserDaoFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,9 +29,11 @@ class UserDaoTest {
     User user;
     User user1 = new User("3", "hakjun", "12344");
     User user2 = new User("4", "hakjun2", "12344");
+    User user3 = new User("5", "hakjun2", "12344");
+
     @BeforeEach
     void setUp() {
-    this.userDao =context.getBean("awsUserDao", UserDao.class);
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
     }
 
 
@@ -42,5 +46,18 @@ class UserDaoTest {
         user = userDao.get("3");
         user = userDao.get("4");
         assertEquals(2, userDao.getCount());
+    }
+
+    @Test
+    @DisplayName("없을때 빈 리스트 리턴하는지, 있을때 개수만큼 리턴하는지")
+    void getAllTest(){
+        userDao.deleteAll();
+        List<User> users = userDao.getAll();
+        assertEquals(0,users.size());
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        users = userDao.getAll();
+        assertEquals(3,users.size());
     }
 }
